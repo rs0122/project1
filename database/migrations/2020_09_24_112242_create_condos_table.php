e49c3ffb70ede124a132d4fd963a576d3d8326e1
斉藤 <rs.0122.ssthe2@hotmail.co.jp>
2020-09-24 11:28:12 +0000

    18th commit

--- database/migrations/2020_09_24_112242_create_condos_table.php
+++ database/migrations/2020_09_24_112242_create_condos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCondoTable extends Migration
+class CreateCondosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCondoTable extends Migration
      */
     public function up()
     {
-        Schema::create('condo', function (Blueprint $table) {
+        Schema::create('condos', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('condo');
             $table->string('price');
@@ -33,6 +33,6 @@ class CreateCondoTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('condo');
+        Schema::dropIfExists('condos');
     }
 }
